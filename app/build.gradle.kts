365f64b08ff58464bbf8bbb3e5bb2ea4fdd31c34
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2024-10-22 10:23:45 +0200

    ra

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.runtime.livedata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,6 +67,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation("androidx.compose.material.icons")
-    implementation("androidx.compose.material:material-icons-extended")
+    implementation(libs.androidx.material.icons.extended)
 }
--- app/src/main/java/com/example/pizzeriathiar/ui/registro/RegistroScreen.kt
+++ app/src/main/java/com/example/pizzeriathiar/ui/registro/RegistroScreen.kt
@@ -1,23 +1,21 @@
 package com.example.pizzeriathiar.ui.registro
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
@@ -25,24 +23,25 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.pizzeriathiar.R
 import com.example.pizzeriathiar.data.ClienteDTO
+import androidx.compose.runtime.livedata.observeAsState
+
 
 
 @Composable
 fun TextoField(teclado:KeyboardType=KeyboardType.Text,label:String){
-    TextField(
+    OutlinedTextField(
         value = "",
         keyboardOptions = KeyboardOptions(keyboardType =  teclado ),
         modifier = Modifier.fillMaxWidth().padding(16.dp),
         label = { Text(label) },
         trailingIcon = {if (teclado == KeyboardType.Password)
-            Icon(
-            modifier = Modifier.size(100.dp),
-            imageVector = Icons.Filled.Favorite,
-            contentDescription = ""
-        ) },
+            IconButton(onClick = {}) { Icon(
+                imageVector = Icons.Filled.Visibility,
+                contentDescription = "")}},
         onValueChange = {}
     )
 }
+
 
 @Composable
 fun PantallaRegistro(registroViewModel: RegistroViewModel){
@@ -64,8 +63,6 @@ fun PantallaRegistro(registroViewModel: RegistroViewModel){
             TextoField(KeyboardType.Password,"Password")
 
             Button(onClick = {}, modifier = Modifier.fillMaxWidth().padding(80.dp)) { Text("Registar") }
-
-
 
         }
     }
