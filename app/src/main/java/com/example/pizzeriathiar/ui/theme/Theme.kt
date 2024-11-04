ebd5a3b2c4c4ea4148ffe10585adb5e491c35f36
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2024-11-04 09:34:32 +0100

    fa

--- app/src/main/java/com/example/pizzeriathiar/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pizzeriathiar/ui/theme/Theme.kt
@@ -260,12 +260,7 @@ fun PizzeriaThiarTheme(
     content: @Composable() () -> Unit
 ) {
   val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-          val context = LocalContext.current
-          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      
-      darkTheme -> darkScheme
+        darkTheme -> darkScheme
       else -> lightScheme
   }
 
